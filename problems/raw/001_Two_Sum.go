a820b1939b4584561d288d296b28cd793faa795d
Toshiaki Ezaki <toshiaki.ezaki@gmail.com>
2025-05-27 01:33:40 -0300

    Added raw and optimized solutions using Golang for two sum problem

---
+++ problems/raw/001_Two_Sum.go
@@ -0,0 +1,10 @@
+func twoSum(nums []int, target int) []int {
+	for i := 0; i < len(nums); i++ {
+		for j := i + 1; j < len(nums); j++ {
+			if nums[j] == target-nums[i] {
+				return []int{i, j}
+			}
+		}
+	}
+	return []int{}
+}
